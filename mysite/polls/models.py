8f291660e048ba2fcd2ebb33378667e803680f4e
huy ngo <huyngopt1994@gmail.com>
2017-08-13 23:19:34 +0700

    fix database

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -9,6 +9,6 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
 
 class Choice(models.Model):
-    question = models.Foreignkey(Question, on_delete=models.CASADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
