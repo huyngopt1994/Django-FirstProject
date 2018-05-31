9eb97747f65157a16381ebc55ea58eb9574cf825
Huy Ngo <huyngopt1994@gmail.com>
2018-05-31 21:23:48 +0700

    add session words

--- dojo_bootcamp/main/main/settings.py
+++ dojo_bootcamp/main/main/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.first_app',
+    'apps.session_words'
 ]
 
 MIDDLEWARE = [
--- dojo_bootcamp/main/main/urls.py
+++ dojo_bootcamp/main/main/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('apps.first_app.urls'))
+    url(r'^', include('apps.first_app.urls')),
+    url(r'^session_words/', include('apps.session_words.urls'))
 ]
 
