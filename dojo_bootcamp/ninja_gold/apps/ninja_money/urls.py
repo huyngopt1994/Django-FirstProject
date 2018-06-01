24ff4eab8e7a9739b107c488de22897c9ee96e32
Huy Ngo <huyngopt1994@gmail.com>
2018-06-01 18:38:25 +0700

    add njnja money

---
+++ dojo_bootcamp/ninja_gold/apps/ninja_money/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+from . import views
+urlpatterns = [
+	url('^$', views.index ),
+	url('^process_money$', views.process_money)
+]
---
+++ dojo_bootcamp/ninja_gold/apps/ninja_money/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+	pass
+
+def process_money(request):
+	pass
