68a0ad60f97344c1662144db5c8b5ee7959b8b26
Huy Ngo <huyngopt1994@gmail.com>
2018-06-01 16:10:48 +0700

    add amadon

--- dojo_bootcamp/amadon/apps/shop/urls.py
+++ dojo_bootcamp/amadon/apps/shop/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 urlpatterns =  [
 	url(r'^$', views.index),
+    url(r'^buy/(?P<id>[0-9])', views.buy),
 	url(r'^checkout$', views.checkout),
-	url(r'^buy/(?P<number>[0-9])$'),
+	url(r'^clear$', views.clear)
 ]
--- dojo_bootcamp/amadon/apps/shop/views.py
+++ dojo_bootcamp/amadon/apps/shop/views.py
@@ -1,11 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+#build dictionary to store a products
+PRODUCT = [
+	{'name':'Dojo Tshirt',
+	 'price': 19.99,
+	 'id': 1,
+	 },
+    {'name':'Dojo Sweater',
+	 'price': 29.99,
+	 'id': 2,
+	 },
+    {'name':'Dojo Cup',
+	 'price': 4.99,
+	 'id': 3,
+	 },
+    {'name':'Algorithm Book',
+	 'price': 40.99,
+	 'id': 4,
+	 }
+]
 # Create your views here.
 def index(request):
-	return render(request,'shop/index.html')
+	context= {
+		'items':PRODUCT
+	}
+	return render(request,'shop/index.html',context)
 
 def checkout(request):
+
 	return render(request,'shop/checkout.html')
 
-def buy(request,number):
-	pass
+def buy(request,id):
+	print(id)
+	if request.method == 'POST':
+		for item in PRODUCT:
+			print(item['id'])
+			if item['id'] == int(id):
+				print('say oh yeah')
+				price = item['price']
+				break
+		else:
+			return redirect('/amadon')
+		quantity = int(request.POST['quantity'])
+		request.session['money'] = quantity * price
+		request.session['price'] = price
+		if 'total_money' not in request.session:
+			request.session['total_money'] = 0
+		if 'total_quantity' not in request.session:
+			request.session['total_quantity'] = 0
+		request.session['total_money'] += request.session['money']
+		request.session['quantity'] = quantity
+		request.session['total_quantity'] += quantity
+		return redirect('/amadon/checkout')
+	return redirect('/amadon')
+
+
+def clear(request):
+	for key in list(request.session.keys()):
+		del request.session[key]
+	return redirect('/amadon')
